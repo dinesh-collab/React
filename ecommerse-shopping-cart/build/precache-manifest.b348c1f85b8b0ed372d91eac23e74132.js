a1f085fcd961b972abf9600bbcedc2313ad3a4e5
DineshYeligandla <61127975+dinesh-collab@users.noreply.github.com>
2025-03-15 18:12:09 -0500

    updating build

---
+++ ecommerse-shopping-cart/build/precache-manifest.b348c1f85b8b0ed372d91eac23e74132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b4fd0ebd5b3309401591fc20f5123d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "261d2e2367ca3b616999",
+    "url": "/static/css/main.d223bfe1.chunk.css"
+  },
+  {
+    "revision": "6bf6cc759b00a3576f6f",
+    "url": "/static/js/2.177fcc96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.177fcc96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261d2e2367ca3b616999",
+    "url": "/static/js/main.feebb599.chunk.js"
+  },
+  {
+    "revision": "fbf32a1bfdd21dd35f1d",
+    "url": "/static/js/runtime-main.1b3557a3.js"
+  }
+]);
